3879b7e7e9f7e01a73727bd2519744fbe26ece95
NatiAtske <86081492+NatiAtske@users.noreply.github.com>
2022-06-25 13:46:23 -0400

    Update TemplateFile.cs

--- TemplateFile.cs
+++ TemplateFile.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+/**
+ * This is a scene loader file. After applying it to the game object, it will work.
+ */
 public class SceneLoader : MonoBehaviour
 {
     public void loadScene(string sceneName)
